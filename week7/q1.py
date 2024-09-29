5650c858d4dc0241719bf4387776918f3ad8b277
JJET88 <aungnyithegreat@gmail.com>
2024-09-29 20:58:55 +0700

    Revert "version 1"
    This reverts commit ef93d3f514dec05cd808f81a8d33046a298607ed.

--- week7/q1.py
+++
@@ -1,9 +0,0 @@
-def TriangleArea(H,W):
-    Triangle =0.5 * H * W
-    return Triangle
-
-Height =float(input("Enter Height: "))
-Width =float(input("Enter Width: "))
-
-# print("Triangle Area =",TriangleArea(Height,Width))
-print(f"Triangle Area = {TriangleArea(Height,Width)}")
